e00f86400f8001a9e9c824aee8f5b98306b753b4
adwait2007 <74483768+adwait2007@users.noreply.github.com>
2020-12-23 19:56:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,85 +1,197 @@
-const Engine=Matter.Engine;
-const World=Matter.World;
+const Engine = Matter.Engine
+const World= Matter.World
+const Bodies = Matter.Bodies
 const Body=Matter.Body;
-const Bodies=Matter.Bodies;
+const Constraint = Matter.Constraint
 
-var engine,world;
-var ground;
+var engine, world;
+var block1, block2, block3, block4, block5, block6, block7, block8, block9;
+var block10,block11,block12, block13, block14;
+var player;
+var ground1, ground2, ground3;
 
-var particles=[];
-var plinkos=[];
-var divisions=[];
-var plinkos2=[];
-var plinkos3=[];
-var plinkos4=[];
+var block15, block16, block17, block18, block19, block20, block21;
+var block22;
+var sling;
+var player;
 
-var divisionHeight=300;
+var ball;
+var img;
+var score=0;
+var score;
 
-function setup(){
-  createCanvas(480,800);
 
-  engine=Engine.create();
-  world=engine.world;
 
-  ground=new Ground(240,795,width,10);
-
-  for(var k=0;k<=width;k=k+80){
-    divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight))
-  }
-
-  for(var j=40;j<=width;j=j+50){
-    plinkos.push(new Plinko(j,75,10));
-  }
-
-  for(var p=15;p<=width;p=p+50){
-    plinkos2.push(new Plinko(p,175,10));
-  }
-
-  for(var c=40;c<=width;c=c+50){
-    plinkos3.push(new Plinko(c,275,10));
-  }
-
-  for(var n=15;n<=width;n=n+50){
-    plinkos4.push(new Plinko(n,375,10));
-  }
- 
+function setup() {
+  createCanvas(1500,600);
+  engine = Engine.create();
+    world = engine.world;
 
   
+  block1 = new Block(600,260,30,40);
+  block2 = new Block(570,260,30,40);
+  block3 = new Block(540,260,30,40);
+  block4 = new Block(630,260,30,40);
+  block5 = new Block(660,260,30,40);
+
+
+
+  block6 = new Block(585,220,30,40);
+  block7 = new Block(555,220,30,40);
+  block8 = new Block(615,220,30,40);
+  block9 = new Block(645,220,30,40);
+
+
+
+  bolck10 = new Block(600,170,30,40);
+  block11 = new Block(570,180,30,40);
+  block12 = new Block(630,180,30,40);
+
+
+  block13 = new Block(600,140,30,40);
+
+  ground1 = new Ground(600,285,200,10);
+  ground2 = new Ground(900,195,200,10);
+  ground3 = new Ground(750, 600, 1500, 10)
+
+
+  block14 = new Block(900,170,30,40);
+  block15 = new Block(930,170,30,40);
+  block16 = new Block(870,170,30,40);
+  block17 = new Block(840,170,30,40);
+  block18 = new Block(960,170,30,40);
+
+  block19 = new Block(900,140,30,40);
+  block20 = new Block(930,140,30,40);
+  block21 = new Block(870,140,30,40);
+
+  block22 = new Block(900,110,30,40);
+
+  player = new Player(50,200,30,30);
+
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  sling = new Chain(this.ball,{x:150, y:160});
+ 
 
 }
 
-function draw(){
-  background("black");
+function draw() {
+  
+  Engine.update(engine);
+  
 
-  for(var k=0;k < divisions.length;k++){
-     divisions[k].display();
-  }
+  fill("purple");
+  text("score :"+score,790,40);
+  
 
-  for(var j =0;j < plinkos.length;j++){
-    plinkos[j].display();
-  }
 
-  for(var p =0;p < plinkos2.length;p++){
-    plinkos2[p].display();
-  }
 
-  for(var c =0;c < plinkos3.length;c++){
-    plinkos3[c].display();
-  }
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  
 
-  for(var n =0;n < plinkos4.length;n++){
-    plinkos4[n].display();
-  }
+  fill("lightBlue");
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  
 
-  if(frameCount%90===0){
-    particles.push(new Particle(random(width/2-10,width/2+10),10,10))
-  }
+  fill("lightPink");
+  bolck10.display();
+  block11.display();
+  block12.display();
+  fill(rgb(47, 48, 48));
 
-  for(var e =0;e < particles.length;e++){
-    particles[e].display();
-   
-  }
 
-  ground.display();
+  block13.display();
 
+  fill(rgb(135, 205, 236));
+  
+  block14.display();
+  block15.display();
+  block16.display();
+  block17.display();
+  block18.display();
+  fill("lightGreen");
+  block19.display();
+  block20.display();
+  block21.display();
+  fill("lime");
+  block22.display();
+
+  ground1.display();
+  ground2.display();
+  ground3.display();
+
+
+
+  sling.display();
+
+  block1.score();
+  block2.score();
+  block3.score();
+  block4.score();
+  block5.score();
+  block6.score();
+  block7.score();
+  block8.score();
+  block9.score();
+  block10.score();
+  block11.score();
+  block12.score();
+  block13.score();
+  block14.score();
+  block15.score();
+  block16.score();
+  block17.score();
+  block18.score();
+  block19.score();
+  block20.score();
+  block21.score();
+  block22.score();
+  
+  
 }
+
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+  sling.fly();
+}
+
+function keyPressed(){
+	if(keyCode === 32)
+	{
+		Matter.Body.setPosition(player.body,{x:235, y:420})
+		chain.attach(player.body);
+	}
+}
+
+async function getBackgroundImg(){
+    
+  var response=await fetch("http://worldclockapi.com/api/json/est/now");
+  var responseJSON=await response.json();
+  console.log(responseJSON);
+  var datetime = responseJSON.currentDateTime
+  var hour = datetime.slice(11,13);
+   if(hour>=06 && hour>=19){
+    background("white")
+   }
+   else{
+      
+      background("black")
+   }
+  ;
+}
+
+ 
